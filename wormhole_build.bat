2ee77b950b734e9b33350918dbac5347f855c6bf
Parkjongho <wfjo852@c2monster.com>
2020-03-09 13:31:17 +0900

    icon 추가

---
+++ wormhole_build.bat
@@ -0,0 +1,1 @@
+H:\Park_doc\python\wh_update\venv\Scripts\pyinstaller --onefile --icon H:\Park_doc\python\wh_update\icon\wh_update_tool.ico --noconsole H:\Park_doc\python\wh_update\wormhole_update_tool.py
